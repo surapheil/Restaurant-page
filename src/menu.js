3a107e1ee06b790853128821d00d4590dbac33a9
surafel <surafellamesgin@gmail.com>
2023-12-29 15:18:00 +0300

    added the menu function

---
+++ src/menu.js
@@ -0,0 +1,68 @@
+const menuCont = document.createElement('div')
+function foodMenu(){
+    const item1 = document.createElement('div');
+    item1.classList.add('item1');
+    const item1Img = document.createElement('img');
+    item1Img.classList.add('item1Img');
+    item1Img.src = './assets/c2dd5d01-6710-4db1-9c6f-892ef4e84e4c.jpeg';
+    const item1Description = document.createElement('p');
+    item1Description.classList.add('disc1');
+    item1.appendChild(item1Img);
+    item1.appendChild(item1Description);
+
+    //---item 2
+    const item2 = document.createElement('div');
+    item2.classList.add('item1');
+    const item2Img = document.createElement('img');
+    item2Img.classList.add('item1Img');
+    item2Img.src = './assets/f679badc-ca65-4305-851c-d7189d5d699d.jpeg';
+    const item2Description = document.createElement('p');
+    item2Description.classList.add('disc1');
+    item2.appendChild(item2Img);
+    item2.appendChild(item2Description);
+
+    // ---item 3
+    const item3 = document.createElement('div');
+    item3.classList.add('item1');
+    const item3Img = document.createElement('img');
+    item3Img.classList.add('item1Img');
+    item3Img.src = './assets/c2dd5d01-6710-4db1-9c6f-892ef4e84e4c.jpeg';
+    const item3Description = document.createElement('p');
+    item3Description.classList.add('disc1');
+    item3.appendChild(item3Img);
+    item3.appendChild(item3Description);
+
+    // ---item 4
+    const item4 = document.createElement('div');
+    item4.classList.add('item1');
+    const item4Img = document.createElement('img');
+    item4Img.classList.add('item1Img');
+    item4Img.src = './assets/c2dd5d01-6710-4db1-9c6f-892ef4e84e4c.jpeg';
+    const item4Description = document.createElement('p');
+    item4Description.classList.add('disc1');
+    item4.appendChild(item4Img);
+    item4.appendChild(item4Description);
+
+    // ---item 5
+    const item5 = document.createElement('div');
+    item5.classList.add('item1');
+    const item5Img = document.createElement('img');
+    item5Img.classList.add('item1Img');
+    item5Img.src = './assets/c2dd5d01-6710-4db1-9c6f-892ef4e84e4c.jpeg';
+    const item5Description = document.createElement('p');
+    item5Description.classList.add('disc1');
+    item5.appendChild(item5Img);
+    item5.appendChild(item5Description);
+
+    // ---item 6
+    const item6 = document.createElement('div');
+    item6.classList.add('item1');
+    const item6Img = document.createElement('img');
+    item6Img.classList.add('item1Img');
+    item6Img.src = './assets/c2dd5d01-6710-4db1-9c6f-892ef4e84e4c.jpeg';
+    const item6Description = document.createElement('p');
+    item6Description.classList.add('disc1');
+    item6.appendChild(item6Img);
+    item6.appendChild(item6Description);
+    
+}
