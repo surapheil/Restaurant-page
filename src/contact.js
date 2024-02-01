8193746c0a9b4e087178a610d28d57f27b511547
surafel <surafellamesgin@gmail.com>
2024-02-01 23:37:47 +0300

    added the effects on the buttons and editeded the contact page

--- src/contact.js
+++ src/contact.js
@@ -1,26 +1,34 @@
 const contact = document.createElement('div');
-import imgMap from './assets/Screenshot from 2024-01-29 23-41-56.png'
+import imgMap from './assets/Screenshot from 2024-01-29 23-41-56.png';
+import phoneIcn from './assets/phone.png';
 contact.classList.add('contactInfo');
 
 function addMap(){
+    const phoneNumber = document.createElement('div');
+    phoneNumber.classList.add('phoneNumber');
     const phone = document.createElement('p');
+    const phoneImg = document.createElement('img');
+    phoneImg.classList.add('phoneIcon');
+    phoneImg.src = phoneIcn;
+
     phone.classList.add('phoneNumber');
-    phone.innerHTML = 'Phone: 0123456789';
-    
+    phone.innerHTML = '0123456789';
+    phoneNumber.appendChild(phoneImg);
+    phoneNumber.appendChild(phone);
     const address = document.createElement('div');
     address.classList.add('address');
     address.innerHTML = `Address: Dembel,123 Boleroad <br>Postal Code: 12345`;;
     const email = document.createElement('a');
     email.classList.add('emailInfo');
     email.href='mailto:test@email.com'
-    email.innerText= "Email: ItIsFakeEmail@email.com";
+    email.innerText= "Email: fakeemail@gmail.com";
 
     const mapImage = document.createElement('img');
     mapImage.classList.add('mapImg');
     mapImage.src = imgMap;
-    mapImage.alt="Contact Map"
+    mapImage.alt="Contact Map";
 
-    contact.appendChild(phone);
+    contact.appendChild(phoneNumber);
     contact.appendChild(email);
     contact.appendChild(address);
     contact.appendChild(mapImage);
--- src/header.js
+++ src/header.js
@@ -4,11 +4,11 @@ function topPage(){
     const menu = document.createElement('button');
     const contact = document.createElement('button');
     const container = document.createElement('div');
-    home.classList.add('home');
+    home.classList.add('home','navBtn');
     home.innerHTML = 'Home';
-    menu.classList.add('menu');
+    menu.classList.add('menu','navBtn');
     menu.innerHTML = 'Menu';
-    contact.classList.add('contact');
+    contact.classList.add('contact','navBtn');
     contact.innerHTML = 'Contact';
     container.classList.add('container');
     container.appendChild(home);
